7b2ece6d8e356e3d33d6ce69bf8152a43a6dad9c
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2025-01-16 22:28:07 +0530

    added cloudinary future for uploading images

--- social-media-backend/routes/submissions.js
+++ social-media-backend/routes/submissions.js
@@ -2,18 +2,9 @@ const express = require("express");
 const multer = require("multer");
 const Submission = require("../models/Submission");
 const path = require("path");
+const { storage } = require("../config/storage");
 
 const router = express.Router();
-
-// Configure multer
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
 
 const upload = multer({ storage });
 
